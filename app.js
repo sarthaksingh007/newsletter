290d2d758ee033c884083b274dd40421023877cc
sarthak <buddysuccess7@gmail.com>
2023-02-05 12:23:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https=require("https");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));//this help to access the file of css and images bcz this webpage is static.
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function (req,res){
+    const firstname=req.body.fname;
+    // console.log(firstname);
+    const lastname=req.body.lname;
+    // console.log(lastname);
+    const email=req.body.email;
+    // console.log(email);
+
+    const data={
+        members: [
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields: {
+                    FNAME:firstname,
+                    LNAME:lastname,
+                }
+            }
+        ]
+    };
+
+    const jsonData=JSON.stringify(data); 
+ 
+    const url = "https://us21.api.mailchimp.com/3.0/lists/d009076be6";
+
+    const options={
+        method: "POST",
+        auth: "sarthak1:56451feb0e8b5819ded93bd20cbb7100-us21"
+    }
+
+    const request = https.request(url,options,function (response) {
+
+        if(response.statusCode === 200 ){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    // request.write(jsonData);
+    request.end();
+
+});
+
+
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server runnings on 3000")
+})
+
+
+
+
+
+
+
+
+
+
+
+//api key
+//56451feb0e8b5819ded93bd20cbb7100-us21
+
+// list id
+// d009076be6
